a4c0605bc1cf5f2b47b5fdf72ad0e2a6bb037e51
Joshua Sundance Bailey <joshua.bailey@swca.com>
2023-06-26 13:49:55 -0400

    Create COVERAGE.md

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,5 @@ set -euo pipefail
 IFS=$'\n\t'
 
 coverage run -m pytest
-coverage report
-coverage html
+coverage report --format markdown --no-skip-covered --skip-empty --show-missing > COVERAGE.md
 coverage-badge -fo coverage.svg
