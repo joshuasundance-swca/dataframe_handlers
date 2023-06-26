fc94f588f62a2944ff636f26273769077a592b0e
Joshua Sundance Bailey <joshua.bailey@swca.com>
2023-06-25 21:28:22 -0400

    bump version 0.0.1 -> 0.0.2

--- dataframe_handlers/__init__.py
+++ dataframe_handlers/__init__.py
@@ -2,7 +2,7 @@ from typing import Any, Optional, Type
 
 from .base import BaseDataFrameHandler
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __all__ = ["BaseDataFrameHandler"]
 
 dispatch_dict = {}
